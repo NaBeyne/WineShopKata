78bdfce9804435ba521e5d31db4b2b80fa336410
nathanBeyne <nathan.beyne@iqgeo.com>
2025-03-21 17:47:37 +0100

    Create separate methods for all kinds of wine

--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -1,8 +1,6 @@
 package be.athumi
 
 private const val WINE_BREWED_BY_ALEXANDER_THE_GREAT = "Wine brewed by Alexander the Great"
-private const val BOURDEAUX_CONSERVATO = "Bourdeaux Conservato"
-private const val BOURGOGNE_CONSERVATO = "Bourgogne Conservato"
 
 private const val EVENT_IDENTIFIER = "Event"
 private const val CONSERVATO_IDENTIFIER = "Conservato"
@@ -22,46 +20,71 @@ class WineShop(var items: List<Wine>) {
     }
 
     private fun updateWine(wine: Wine) {
-        if (wine.name != BOURDEAUX_CONSERVATO && wine.name != BOURGOGNE_CONSERVATO && !wine.name.startsWith(EVENT_IDENTIFIER)) {
-            if (wine.price > MIN_PRICE && wine.name != WINE_BREWED_BY_ALEXANDER_THE_GREAT) {
-                wine.price--
-            }
-        } else {
-            if (wine.price < MAX_PRICE) {
-                wine.price++
+        when {
+            wine.name.contains(CONSERVATO_IDENTIFIER) -> handleConservatoWine(wine)
+            wine.name.startsWith(EVENT_IDENTIFIER) -> handleEventWine(wine)
+            wine.name == WINE_BREWED_BY_ALEXANDER_THE_GREAT -> handleWineBrewedByAlexanderTheGreat(wine)
+            else -> handleRegularWine(wine)
+        }
+    }
 
-                if (wine.name.startsWith(EVENT_IDENTIFIER)) {
-                    if (wine.expiresInYears < EVENT_YEARS_FIRST_PRICE_INCREASE && wine.price < MAX_PRICE) {
-                        wine.price++
-                    }
-
-                    if (wine.expiresInYears < EVENT_YEARS_SECOND_PRICE_INCREASE && wine.price < MAX_PRICE) {
-                        wine.price += 2
-                    }
-                }
-            }
+    private fun handleConservatoWine(wine: Wine) {
+        if (wine.price < MAX_PRICE) {
+            wine.price++
         }
 
-        if (wine.name != WINE_BREWED_BY_ALEXANDER_THE_GREAT) {
-            wine.expiresInYears--
-        } else if (wine.price < MIN_PRICE) {
+        wine.expiresInYears--
+
+        if (wine.expiresInYears < MIN_YEARS && wine.price < MAX_PRICE) {
+            wine.price++
+        }
+
+        if (wine.price < MIN_PRICE) {
             wine.price = MIN_PRICE
         }
+    }
 
-        if (wine.expiresInYears < MIN_YEARS) {
-            if (!wine.name.contains(CONSERVATO_IDENTIFIER)) {
-                if (!wine.name.contains(EVENT_IDENTIFIER)) {
-                    if (wine.price > MIN_PRICE && wine.name != WINE_BREWED_BY_ALEXANDER_THE_GREAT) {
-                        wine.price--
-                    }
-                } else {
-                    wine.price = MIN_PRICE
-                }
-            } else {
-                if (wine.price < MAX_PRICE) {
+    private fun handleEventWine(wine: Wine) {
+        if (wine.price < MAX_PRICE) {
+            wine.price++
+
+            if (wine.name.startsWith(EVENT_IDENTIFIER)) {
+                if (wine.expiresInYears < EVENT_YEARS_FIRST_PRICE_INCREASE && wine.price < MAX_PRICE) {
                     wine.price++
                 }
+
+                if (wine.expiresInYears < EVENT_YEARS_SECOND_PRICE_INCREASE && wine.price < MAX_PRICE) {
+                    wine.price += 2
+                }
             }
+
+            if (wine.expiresInYears < MIN_YEARS) {
+                wine.price = MIN_PRICE
+            }
+        }
+
+        wine.expiresInYears--
+
+        if (wine.expiresInYears < MIN_YEARS) {
+            wine.price = MIN_PRICE
+        }
+    }
+
+    private fun handleWineBrewedByAlexanderTheGreat(wine: Wine) {
+        if (wine.price < MIN_PRICE) {
+            wine.price = MIN_PRICE
+        }
+    }
+
+    private fun handleRegularWine(wine: Wine) {
+        if (wine.price > MIN_PRICE) {
+            wine.price--
+        }
+
+        wine.expiresInYears--
+
+        if (wine.expiresInYears < MIN_YEARS && wine.price > MIN_PRICE) {
+            wine.price--
         }
 
         if (wine.price < MIN_PRICE) {
